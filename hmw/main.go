efd60d199aba2a9d3ca0596ec556ba3ed49f49f3
m <m>
2024-01-18 18:56:56 +0500

    test

---
+++ hmw/main.go
@@ -0,0 +1,43 @@
+package main
+
+
+
+import "fmt"
+
+func main() {
+    // a :=2
+	// fmt.Print(a*4)
+
+	// a := 4
+	// fmt.Print(a*a)
+
+	a := 5
+	b := 6
+	s := a*b
+	p := 2*(a+b)
+	fmt.Print(s,p)
+
+
+    // var d float64  = 3.14
+	// var a float64  = 3.14
+	// m := d*a
+	// fmt.Print(m)
+
+	// a := 4
+	// b := a*a*a
+	// s := 6*a*a
+	// fmt.Print(b,s)
+
+	// a := 5
+	// b := 7
+	// c := 6
+	// v := a*b*c
+	// s := 2*(a*b+b*c+a*c)
+	// fmt.Print(v,s)
+
+    // r := 19 
+	// l := 20
+	// b := 2*l*r
+	// s := l*r*r
+	// fmt.Print(b,s)
+}
