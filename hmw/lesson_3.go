be54339f1860a5e68c866fba0e772cf70ff6f436
m <m>
2024-01-18 23:46:35 +0500

    test

--- hmw/lesson_3.go
+++ hmw/lesson_3.go
@@ -1,17 +1,25 @@
-package main 
+package main
 
 import "fmt"
 
-func main(){
-var name string 
-fmt.Scan("Your name:",name)
+func main() {
 
-var age int
-fmt.Scan("Your age:",age)
 
-var work string
-fmt.Scan("Your work:",work)
-fmt.Printf("My name:%s","My age:%d","My work:%s",)
+
+
+	var name string
+	fmt.Scan(&name)
+
+	var age int
+	_,err :=fmt.Scan(&age)
+	fmt.Println(err)
+
+	var work string
+	fmt.Scan(&work)
+	fmt.Printf("My name:%s\n My age:%d\n My work:%s", name, age, work)
+
+	formatedStr :=fmt.Sprintf("My name:%s,My age:%d, My work:%s", name, age, work)
+	fmt.Print("Creat:",formatedStr)
 }
 
-//my name, my age, my work
+
