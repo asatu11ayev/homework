afddccf42d59467c022dce005d57bba54e023858
m <m>
2024-01-20 20:38:05 +0500

    lesson_5.go

---
+++ hmw/lesson_5.go
@@ -0,0 +1,151 @@
+package main 
+
+import "fmt"
+
+func main(){
+	
+    // a := 1
+	// switch a {
+	// case 1:
+	// 	fmt.Println("duwanba")
+	// case 2:
+	// 	fmt.Println("sewanba")
+	// case 3:
+	// 	fmt.Println("chorwanba")
+	// case 4:
+	// 	fmt.Println("paywanba")
+	// case 5:
+	// 	fmt.Println("juma")
+	// case 6:
+	// 	fmt.Println("wanba")
+	// case 7:
+	// 	fmt.Println("yakwanba")
+	// }
+
+    // a := 10
+	// b := 23
+	// plus :=0
+	// for a<b{
+	// 	fmt.Println("a:",a)
+	// 	a++
+	// 	plus++
+	// }
+	// fmt.Println("plus:",plus)
+
+//    k :=5
+//    n := 8
+// 	for i := 1; i <=n; i++{
+//       fmt.Println(i,k)
+// 	}
+	
+//    a := 11
+//    b := 10
+//    if a>b{
+// 	fmt.Println("a kotta b dan",a)
+//    }else if b>a{
+// 	fmt.Println("b kotta a dan",b)
+//    }else if a==b{
+// 	fmt.Println("a ham b ham teng",a,b)
+//    }
+      
+       
+    //  a := 15
+	//  b := 100
+	//  if a<b{
+	// 	fmt.Println("tartib raqami 1")
+	//  }else if a>b{
+	// 	fmt.Println("tartib raqami 2")
+	//  }
+
+
+//    a := 10
+//    b := 10
+//    if a>b{
+// 	fmt.Println("a kotta b dan")
+//    }else if b>a{
+// 	fmt.Println("b kotta a dan")
+//    }else if a==b{
+// 	fmt.Println("a ham b ham teng")
+//    }
+
+	// a := -0
+	// b := -4
+	// c := -6
+	// if a>-0{
+	//   fmt.Println("Mustab son 3 ta va manfiy sondan yoq")
+	// }else if a>-0||b>-0{
+	//    fmt.Println("Musbat son 2 ta va manfiy son 1 ta")
+	// }else if a>-0||b>-0||c>-0{
+	//    fmt.Println("Musbat son 1 ta va manfiy son 2 ta")
+	// }else{
+	//    fmt.Println("Musbat  son mavjud emas manfiy sondan 3 ta")
+	// }
+
+    //  a := 0
+	//  b := 4
+	//  c := 6
+	//  if a>-0{
+    //    fmt.Println("Mustab son 3 ta")
+	//  }else if a>-0||b>=-0{
+	// 	fmt.Println("Musbat son 2 ta")
+	//  }else if a>-0||b>=-0||c>=-0{
+	// 	fmt.Println("Musbat son 1 ta")
+	//  }else{
+	// 	fmt.Println("Musbat son mavjud emas")
+	//  }
+
+	// a := 0
+	//  if a>0{
+	// 	fmt.Println("Positive",a+1)
+	//  }else if a==0{
+	// 	fmt.Println(a+10)
+	//  }else{
+	// 	fmt.Println("Negative", a-2)
+	//  }
+
+	// a := -1
+	//  if a>=0{
+	// 	fmt.Println("Positive",a+1)
+	//  }else{
+	// 	fmt.Println("Negative",a-2)
+	//  }
+
+    //  a := 5
+	//  if a>=0{
+	// 	fmt.Println("Positive",a+1)
+	//  }else{
+	// 	fmt.Println("Negative")
+	//  }
+
+    //  age := 15
+	//  if age % 2 == 0{
+	// 	fmt.Println(age)
+	// 	fmt.Scanln(&age)
+	//  }else if age%2!=0{
+	// 	fmt.Println(age)
+	// 	fmt.Scanln(&age)
+	//  }
+
+    // a := 6
+	// if a%2==0{
+	// 	fmt.Println("a ning kvadrati",a*a)
+	// }else {
+	// 	fmt.Println("a ning kubi",a*a*a)
+	// }
+
+    // a := 2000
+	// if a%400==0{
+	// 	fmt.Println("Kabisa yili")
+	// }else{
+	// 	fmt.Println("Kabisa yili emas")
+	// }
+
+	// a := 33
+	// if a%3==0 && a%5==0 {
+	// 	fmt.Println("FIZZBUZZ")
+	// }else if  a%3==0 {
+	// 	fmt.Println("FIZZ")
+	// }else if  a%5==0{
+	// 	fmt.Println("BUZZ")
+	// }
+}
