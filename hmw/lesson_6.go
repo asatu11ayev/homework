11a4a27c0c014477684f6c65832e2705682c9f3e
m <m>
2024-01-23 17:55:17 +0500

    lesson_6.go

---
+++ hmw/lesson_6.go
@@ -0,0 +1,23 @@
+package main
+
+// import "fmt"
+
+// func printReversed(a,b int){
+// 	if a<b{
+// 		for i := a; i <=b; i++ {
+// 			fmt.Print(i,",")
+// 		}
+// 	}else if a>b{
+// 		for i := a; i >= b; i-- {
+// 			fmt.Print(i,",")
+			
+// 		}
+// 	}
+
+
+// }
+// func main (){
+//  a := 7
+//  b := 5
+//  printReversed(a,b)
+// }
