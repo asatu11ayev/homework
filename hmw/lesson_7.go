b092978d3aea5b6ca74015536d0500f0e5fc75c5
m <m>
2024-01-24 20:26:21 +0500

    lesson_8.go

--- hmw/lesson_7.go
+++ hmw/lesson_7.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+// import (
+//   "fmt"
+//   "strings"
+//   "unicode"
+// )
 
-func main ()  {
-	
-}
+// func main() {
+//     fmt.Println(findIngLyWords("Walking is healthy Swiftly they ran."))
+// }
+
+// func findIngLyWords(text string) []string {
+//   words := strings.Fields(text)
+
+//   result := []string{}
+
+//   for _, word := range words {
+//     if len(words) > 2 && unicode.IsUpper(rune(word[0])) && strings.HasSuffix(word, "ing") || strings.HasSuffix(word, "ly")  {
+//       result = append(result, word)
+//     }
+//   }
+//   return result
+// }
---
+++ hmw/lesson_8.go
@@ -0,0 +1,25 @@
+package main
+
+// import "fmt"
+
+// func main() {
+
+
+//   n := 0
+//   fmt.Scan(&n)
+//   strings := make([]string, 0,n)
+//   for i := 0; i < n; i++ {
+// 	 tempString := ""
+// 	 fmt.Scan(&tempString)
+// 	 strings = append(strings,tempString)
+//   }
+//   fmt.Println(strings)
+
+
+// 	arr1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+// fmt.Println(arra(arr1))
+// }
+// func arra(arr1 []int) int {
+// 	arra := arr1[1]+ arr1[2]
+// 	return arra
+// }
